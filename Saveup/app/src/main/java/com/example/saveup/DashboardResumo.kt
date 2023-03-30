fce125d4bce5598c814f2ae5299ebec51a1b75ad
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2023-03-30 20:13:17 -0300

    iniciando desenvolvimento de grafico inline

--- Saveup/app/src/main/java/com/example/saveup/DashboardResumo.kt
+++ Saveup/app/src/main/java/com/example/saveup/DashboardResumo.kt
@@ -3,12 +3,13 @@ package com.example.saveup
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
 
-class MainActivity : AppCompatActivity() {
+class DashboardResumo : AppCompatActivity() {
     lateinit var lineList:ArrayList<Entry>
     lateinit var lineDataSet: LineDataSet
     lateinit var lineData: LineData
@@ -17,21 +18,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dashboard_resumo)
         lineList = ArrayList()
         lineList.add(Entry(10f,100f))
-        lineList.add(Entry(20f,300f))
-        lineList.add(Entry(30f,200f))
-        lineList.add(Entry(40f,600f))
-        lineList.add(Entry(50f,500f))
-        lineList.add(Entry(60f,900f))
+        lineList.add(Entry(10f,100f))
+        lineList.add(Entry(10f,100f))
+        lineList.add(Entry(10f,100f))
+        lineList.add(Entry(10f,100f))
+        lineList.add(Entry(10f,100f))
 
         lineDataSet = LineDataSet(lineList, "SaveUp")
         lineData = LineData(lineDataSet)
         R.layout.activity_dashboard_resumo
         val line_chart: LineChart = findViewById(R.id.linear_chart)
         line_chart.data = lineData
-        lineDataSet.color = Color.BLACK
-//        lineDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
+        lineDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
         lineDataSet.valueTextColor = Color.BLUE
-        lineDataSet.valueTextSize = 15f
-        lineDataSet.setDrawFilled(true)
+        lineDataSet.valueTextSize = 20f
     }
 }
