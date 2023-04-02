530362ad1e897b1808836bf9b9e89b9de5b923a1
costa-edu <eduardo.ecosta@linx.com.br>
2023-04-01 21:53:08 -0300

    menu lateral completo

--- Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
+++ Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.navigation.Navigator
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.menu_lateral)
     }
 }
