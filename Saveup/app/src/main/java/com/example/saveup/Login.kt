078cfd4c1d75e798828aa7ebd69df04d6b689d0a
Guilherme Carneiro <guilherme.martin@valemobi.com.br>
2023-04-03 19:59:37 -0300

    primeira navegacao

--- Saveup/app/src/main/java/com/example/saveup/Login.kt
+++ Saveup/app/src/main/java/com/example/saveup/Login.kt
@@ -3,20 +3,28 @@ package com.example.saveup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import androidx.navigation.Navigator
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+//        findViewById<Button>(R.id.btSignIn).setOnClickListener {
+//            goToHomePage()
+//        }
 
         findViewById<Button>(R.id.btSignUp).setOnClickListener {
             GoToSignUpPage()
         }
     }
 
-    private fun GoToSignUpPage() {
+//    private fun goToHomePage() {
+//        val homePage = Intent(this, HomePage::class.java)
+//        startActivity(homePage)
+//    }
+
+     private fun GoToSignUpPage() {
         val signUpPage = Intent(this, Cadastro::class.java)
         startActivity(signUpPage)
     }
