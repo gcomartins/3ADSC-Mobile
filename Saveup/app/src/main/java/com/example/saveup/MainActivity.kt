13caa64faf0fabfabec35de628d34ec4bb181530
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2023-04-03 19:33:57 -0300

    estilizando grafico e adicionando cor verde padrao

--- Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
+++ Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.saveup
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 
 class MainActivity : AppCompatActivity() {
     lateinit var lineList:ArrayList<Entry>
-    lateinit var lineDataSet: LineDataSet
+    lateinit var dataSet: LineDataSet
     lateinit var lineData: LineData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +27,54 @@ class MainActivity : AppCompatActivity() {
         lineList.add(Entry(50f,500f))
         lineList.add(Entry(60f,900f))
 
-        lineDataSet = LineDataSet(lineList, "SaveUp")
-        lineData = LineData(lineDataSet)
+        dataSet = LineDataSet(lineList, "Valor guardado por mês")
+        lineData = LineData(dataSet)
         R.layout.activity_dashboard_resumo
         val line_chart: LineChart = findViewById(R.id.linear_chart)
+
+        val verde_bolinha_grafico = ContextCompat.getColor(this, R.color.verde_grafico_inline)
+
         line_chart.data = lineData
-        lineDataSet.color = Color.BLACK
-//        lineDataSet.setColors(*ColorTemplate.JOYFUL_COLORS)
-        lineDataSet.valueTextColor = Color.BLUE
-        lineDataSet.valueTextSize = 15f
-        lineDataSet.setDrawFilled(true)
+        dataSet.color = verde_bolinha_grafico
+        dataSet.valueTextColor = Color.CYAN
+        dataSet.valueTextSize = 15f
+        dataSet.setDrawFilled(true)
+        dataSet.fillColor = Color.parseColor("#4CAF50") // verde claro
+        dataSet.fillAlpha = 30 // opacidade de 0 a 255
+        dataSet.circleRadius = 8f
+        dataSet.circleHoleColor = verde_bolinha_grafico
+        dataSet.circleColors = mutableListOf(verde_bolinha_grafico)
+
+        //Descricao do grafico
+
+        line_chart.legend.setDrawInside(false)
+        line_chart.legend.textColor = Color.WHITE
+        line_chart.legend.form = Legend.LegendForm.CIRCLE
+
+        line_chart.apply {
+            setNoDataText("Nenhum dado disponível no momento")//msg exibida quando não há dados
+            title = "Dinheiro guardado"
+            setNoDataTextColor(Color.GREEN)
+        }
+
+        //removendo grids no fundo do gráfico
+        val xAxis = line_chart.xAxis
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(false)
+        xAxis.valueFormatter
+
+        val yAxisLeft = line_chart.axisLeft
+        yAxisLeft.setDrawGridLines(false)
+        yAxisLeft.setDrawAxisLine(false)
+
+        val yAxisRight = line_chart.axisRight
+        yAxisRight.setDrawGridLines(false)
+        yAxisRight.setDrawAxisLine(false)
+        //Fim removendo grid do fundo
+
+
+
+
+
     }
 }
