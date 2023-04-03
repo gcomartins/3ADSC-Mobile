fb7f5389d5dbc9d9834d6832de5a24f9dabf07f7
mtorokm <MATHEUS.TOROK@bandtec.com.br>
2023-04-03 20:07:33 -0300

    Retirando imports desnecessários

--- Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
+++ Saveup/app/src/main/java/com/example/saveup/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.example.saveup
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
