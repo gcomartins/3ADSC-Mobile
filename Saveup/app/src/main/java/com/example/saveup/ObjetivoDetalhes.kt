ddef41ae01672b54e07b564443751c52b0e37809
Q-Atalien <atauinesouto@outlook.com>
2023-04-10 15:27:35 -0300

    Subindo as telas de objetivos e detalhes de objetivo

---
+++ Saveup/app/src/main/java/com/example/saveup/ObjetivoDetalhes.kt
@@ -0,0 +1,18 @@
+package com.example.saveup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ObjetivoDetalhes : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_objetivo_detalhes)
+
+
+    }
+
+}
