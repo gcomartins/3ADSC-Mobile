7ea946fe360f5cd77965926d6a03b56f54f9da63
costa-edu <eduardo.ecosta@linx.com.br>
2023-04-23 14:03:56 -0300

    fix: implementando cadastro funcional

--- Saveup/app/src/main/java/models/Usuario.kt
+++ Saveup/app/src/main/java/models/Usuario.kt
@@ -1,11 +1,17 @@
 package models
 
-import java.util.Date
+import java.util.*
 
 data class Usuario(
     val email: String,
     val nome: String,
     val senha: String,
-    val dataDeNascimento: String,
+    val dataNascimento: Date,
     val cpf: String,
+)
+data class UsuarioCadastro(
+    val email: String,
+    val nome: String,
+    val senha: String,
+    val dataNascimento: String,
 )
