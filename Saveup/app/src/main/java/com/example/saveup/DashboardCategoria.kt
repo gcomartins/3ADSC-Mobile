6f796e2fd0303d2f88e3cdfbc026d882aa2c71eb
Larrisa Santos <larrisa.santos@VMMACMINI-16.local>
2023-05-28 20:21:06 -0300

    coreções do chart

--- Saveup/app/src/main/java/com/example/saveup/DashboardCategoria.kt
+++ Saveup/app/src/main/java/com/example/saveup/DashboardCategoria.kt
@@ -1,47 +1,98 @@
 package com.example.saveup
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.anychart.AnyChart
-import com.anychart.AnyChartView
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.charts.Pie
 import com.example.saveup.databinding.ActivityDashboardCategoriaBinding
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 
-class DashboardCategoria: AppCompatActivity() {
+class   DashboardCategoria: AppCompatActivity() {
     private lateinit var binding: ActivityDashboardCategoriaBinding
-    private var chart: AnyChartView? = null
-
-    private val salary = listOf(200,300,400,600,500)
-    private val month = listOf("Comidas","Contas","Transporte","Compras","Entreterimento")
-
+    private lateinit var ourPieChart: PieChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardCategoriaBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_dashboard_categoria)
         supportActionBar?.hide()
-        chart = findViewById(R.id.pieChart)
+        ourPieChart = binding.pieChart
+        retrieveRecordsAndPopulateCharts()
+    }
+    fun retrieveRecordsAndPopulateCharts() {
+        //calling the retreiveAnimals method of DatabaseHandler class to read the records
+         val animal = listOf(200,300,400,600,500)
+        //create arrays for storing the values gotten
+        val animalIDArray = Array<Int>(animal.size) { 0 }
+        val animalNameArray = Array<String>(animal.size) { "natgeo" }
+        val animalNumberArray = Array<Int>(animal.size) { 0 }
+        val animalAgeArray = Array<Int>(animal.size) { 5 }
+        val animalGrowthArray = Array<Int>(animal.size) { 0 }
 
-        configChartView()
+        //call the methods for populating the charts
+        populatePieChart(animalNumberArray, animalNameArray)
+
     }
 
-    private fun configChartView() {
-        val pie : Pie = AnyChart.pie()
 
+    private fun populatePieChart(values: Array<Int>, labels: Array<String>) {
+        //an array to store the pie slices entry
+        val ourPieEntry = ArrayList<PieEntry>()
+        var i = 0
 
-        val dataPieChart: MutableList<DataEntry> = mutableListOf()
-
-        for (index in salary.indices){
-            dataPieChart.add(ValueDataEntry(month.elementAt(index),salary.elementAt(index)))
+        for (entry in values) {
+            //converting to float
+            var value = values[i].toFloat()
+            var label = labels[i]
+            //adding each value to the pieentry array
+            ourPieEntry.add(PieEntry(value, label))
+            i++
         }
 
-        pie.data(dataPieChart)
-        chart!!.setChart(pie)
+        //assigning color to each slices
+        val pieShades: ArrayList<Int> = ArrayList()
+        pieShades.add(Color.parseColor("#0E2DEC"))
+        pieShades.add(Color.parseColor("#B7520E"))
+        pieShades.add(Color.parseColor("#5E6D4E"))
+        pieShades.add(Color.parseColor("#DA1F12"))
 
+        //add values to the pie dataset and passing them to the constructor
+        val ourSet = PieDataSet(ourPieEntry, "")
+        val data = PieData(ourSet)
+
+        //setting the slices divider width
+        ourSet.sliceSpace = 1f
+
+        //populating the colors and data
+        ourSet.colors = pieShades
+        ourPieChart.data = data
+        //setting color and size of text
+        data.setValueTextColor(Color.WHITE)
+        data.setValueTextSize(10f)
+
+        //add an animation when rendering the pie chart
+        ourPieChart.animateY(1400, Easing.EaseInOutQuad)
+        //disabling center hole
+        ourPieChart.isDrawHoleEnabled = false
+        //do not show description text
+        ourPieChart.description.isEnabled = false
+        //legend enabled and its various appearance settings
+        ourPieChart.legend.isEnabled = true
+        ourPieChart.legend.orientation = Legend.LegendOrientation.HORIZONTAL
+        ourPieChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        ourPieChart.legend.isWordWrapEnabled = true
+
+        //dont show the text values on slices e.g Antelope, impala etc
+        ourPieChart.setDrawEntryLabels(false)
+        //refreshing the chart
+        ourPieChart.invalidate()
 
     }
 }
+
 
 
 
