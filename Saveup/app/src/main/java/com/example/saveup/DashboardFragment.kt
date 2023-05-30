78e73ce20adf6f197138b4213b575b1f6784af2c
Diego <diegosousa241104@gmail.com>
2023-05-29 21:55:59 -0300

    passando os cards para a recycle

--- Saveup/app/src/main/java/com/example/saveup/DashboardFragment.kt
+++ Saveup/app/src/main/java/com/example/saveup/DashboardFragment.kt
@@ -1,5 +1,7 @@
 package com.example.saveup
 
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
@@ -7,7 +9,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
@@ -19,14 +25,15 @@ import com.github.mikephil.charting.utils.MPPointF
 class DashboardFragment : Fragment() {
 
     private lateinit var ourPieChart: PieChart
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var meuAdapter: CategoriAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
-
+        recyclerView = view.findViewById(R.id.recyclerView)
         ourPieChart = view.findViewById(R.id.pieChartt)
 
         val color = ContextCompat.getColor(requireContext(), R.color.white_ice) // Substitua R.color.my_color pelo ID da cor desejada
@@ -83,7 +90,7 @@ class DashboardFragment : Fragment() {
         entries.add(PieEntry(30f))
         entries.add(PieEntry(20f))
         entries.add(PieEntry(30f))
-        entries.add(PieEntry(5f))
+//        entries.add(PieEntry(5f))
 
         // on below line we are setting pie data set
         val dataSet = PieDataSet(entries, "teste")
@@ -94,7 +101,7 @@ class DashboardFragment : Fragment() {
         dataSet.sliceSpace = 3f
         dataSet.iconsOffset = MPPointF(0f, 40f)
         dataSet.selectionShift = 5f
-        dataSet.sliceSpace = 0f
+        dataSet.sliceSpace = 2f
 
         // add a lot of colors to list
         val colors: ArrayList<Int> = ArrayList()
@@ -122,6 +129,60 @@ class DashboardFragment : Fragment() {
         // loading chart
         ourPieChart.invalidate()
 
+        val dataList = listOf(
+            Categoria("Teste", "A", 20.0, "testeeee"),
+            Categoria("Teste", "B", 20.0, "testeeee"),
+            Categoria("Teste", "C", 20.0, "testeeee"),
+        )
+
+        // Inicialize o Adapter com os dados desejados e defina-o no RecyclerView
+        // O adapter que vai pegar os dados e transformar no grafico de objetivo
+        meuAdapter = CategoriAdapter(dataList, requireContext())
+        recyclerView.adapter = meuAdapter
+
         return view
+    }
+
+
+}
+
+data class Categoria(
+    val titulo: String,
+    val Letra: String,
+    val valorCategoria: Double,
+    val descricao: String,
+)
+
+class CategoriAdapter(
+    private val list: List<Categoria>,
+    private val context: Context
+) : RecyclerView.Adapter<CategoriAdapter.GraficoCategoriaViewHolder>() {
+
+    inner class GraficoCategoriaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //FUNCAO QUE PEGA OS DADOS E TRANSFORMA NO GRAFICO DE OBJETIVO
+        fun bind(objetivo: Categoria){
+            val titulo = itemView.findViewById<TextView>(R.id.titulo)
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            val valorObjetivo = itemView.findViewById<TextView>(R.id.valorObjetivo)
+            val letraObjetivo = itemView.findViewById<TextView>(R.id.card_letra)
+            val grafico = itemView.findViewById<ProgressBar>(R.id.progress_circular)
+
+            titulo.text = objetivo.titulo
+            descricao.text = objetivo.descricao
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GraficoCategoriaViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_deposito_objetivo, parent, false)
+        return GraficoCategoriaViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: GraficoCategoriaViewHolder, position: Int) {
+        val categoria = list[position]
+
+        holder.bind(categoria)
     }
 }
