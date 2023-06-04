ae95b02524dd4dc2b748d57ca4f9ee59352b2d99
Guilherme Carneiro <guilherme.martin@valemobi.com.br>
2023-06-04 15:10:05 -0300

    Adicionando FAB pra abrir Nova Despesa

--- Saveup/app/src/main/java/com/example/saveup/TelaPrincipalActivity.kt
+++ Saveup/app/src/main/java/com/example/saveup/TelaPrincipalActivity.kt
@@ -1,12 +1,14 @@
 package com.example.saveup
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.saveup.databinding.ActivityTelaPrincipalBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TelaPrincipalActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -26,6 +28,13 @@ class TelaPrincipalActivity : AppCompatActivity() {
         binding.bottomNavigation.setupWithNavController(navController)
 
         val fragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            intent = Intent(this, NovaDespesa::class.java)
+            startActivity(intent)
+        }
+
 
 //        val meuFragment = FragmentObjetivo()
 //
